01970e55a31cf2a0ee258c810ba16d652804b183
Ostrovskiy Grigoriy <greevex@gmail.com>
2012-10-16 16:11:57 +0400

    redis driver

---
+++ system/cache/redisCache.php
@@ -0,0 +1,83 @@
+<?php
+namespace grunge\system\cache;
+
+use \grunge\system\systemConfig;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of gFileCache
+ *
+ * @author GreeveX <greevex@gmail.com>
+ */
+class memcacheCache
+implements \grunge\system\interfaces\cache {
+    
+    private $autoCommit = true;
+
+    private $configName = "default";
+
+    /**
+     * @return \Redis
+     */
+    private function _factory()
+    {
+        static $instance;
+        if($instance == null) {
+            $instance = new \Redis();
+            $config = systemConfig::$cache[$this->configName];
+            $instance->pconnect($config['server']['host'], $config['server']['port'], $config['server']['timeout']);
+            $instance->setOption(\Redis::OPT_SERIALIZER, \Redis::SERIALIZER_PHP);
+            $instance->setOption(\Redis::OPT_PREFIX, $config['server']['prefix']);
+        }
+        return $instance;
+    }
+
+    public function commit()
+    {
+
+    }
+    
+    public function __construct($configName)
+    {
+        $this->configName = $configName;
+    }
+
+    public function enableAutoCommit()
+    {
+        $this->autoCommit = true;
+    }
+    
+    public function disableAutoCommit()
+    {
+        $this->autoCommit = false;
+    }
+    
+    public function set($key, $value, $expire = 0)
+    {
+        return $this->_factory()->setex($key, $expire, $value);
+    }
+    
+    public function get($key)
+    {
+        return $this->_factory()->get($key);
+    }
+    
+    public function exists($key)
+    {
+        return $this->_factory()->exists($key);
+    }
+    
+    public function delete($key)
+    {
+        return $this->_factory()->delete($key);
+    }
+    
+    public function clear()
+    {
+        return $this->_factory()->flushDB();
+    }
+}
